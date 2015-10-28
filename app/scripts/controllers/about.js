36c4bd0004fd73443a2442741457482af8f24ad0
Alexandre <alexandre.mangin@viacesi.fr>
2015-10-28 01:22:48 +0100

    1st commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name liaFrontendApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the liaFrontendApp
+ */
+angular.module('liaFrontendApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name liaFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the liaFrontendApp
+ */
+angular.module('liaFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
